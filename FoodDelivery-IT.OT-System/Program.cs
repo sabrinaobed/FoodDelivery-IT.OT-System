4a9bd5f1c75440d48d02529f0060ef34d8283e07
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-09-12 22:53:08 +0200

    Add project files.

---
+++ FoodDelivery-IT.OT-System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodDelivery_IT.OT_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
