de522493117cba120f8bf9fc099ae7e760ea2084
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-09-13 11:04:28 +0200

    added options to orderstaus enum

---
+++ FoodDelivery-IT.OT-System/Enum/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDelivery_IT.OT_System.Enum
+{
+    public enum OrderStatus
+    {
+        Placed = 0,      // Order has been placed by staff on behalf of customer
+        Processing = 1,  // Order is being cooked/prepared
+        Sent = 2         // Order is ready and handed to rider
+    }
+}
