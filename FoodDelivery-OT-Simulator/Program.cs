2d9abb33c1484cee1545f2e967bb0ab4d75d1b51
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-09-13 15:08:10 +0200

    Created a new project Food Delivery OT simulator in same  solution

---
+++ FoodDelivery-OT-Simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FoodDelivery_OT_Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
