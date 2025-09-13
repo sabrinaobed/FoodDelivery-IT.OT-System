08c64d991b1ef5cf940d9b4343a82eefc21aaa4e
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-09-13 13:19:59 +0200

    Added business logic for AuthService

--- FoodDelivery-IT.OT-System/Services/AuthService.cs
+++ FoodDelivery-IT.OT-System/Services/AuthService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FoodDelivery_IT.OT_System.Data;
+using FoodDelivery_IT.OT_System.Interfaces;
+using FoodDelivery_IT.OT_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,27 @@ using System.Threading.Tasks;
 
 namespace FoodDelivery_IT.OT_System.Services
 {
-    internal class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly AppDbContext _db;
+
+        public AuthService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public UserModel? Login(string email, string password)
+        {
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+                return null;
+
+            var user = _db.Users.SingleOrDefault(u => u.Email == email);
+            if (user == null)
+                return null;
+
+            var ok = BCrypt.Net.BCrypt.Verify(password, user.PasswordHash);
+            return ok ? user : null;
+        }
+
     }
 }
