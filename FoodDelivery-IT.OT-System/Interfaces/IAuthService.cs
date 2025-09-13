520a54a40d9071c1379376199c326b4bf203c113
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-09-13 13:10:03 +0200

    Added method in IAuthService interface

--- FoodDelivery-IT.OT-System/Interfaces/IAuthService.cs
+++ FoodDelivery-IT.OT-System/Interfaces/IAuthService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FoodDelivery_IT.OT_System.Models;
 
 namespace FoodDelivery_IT.OT_System.Interfaces
 {
     public interface IAuthService
     {
+        //Returns the logged-in user or null if credentials are invalid.</summary>
+        UserModel? Login(string email, string password);
     }
 }
