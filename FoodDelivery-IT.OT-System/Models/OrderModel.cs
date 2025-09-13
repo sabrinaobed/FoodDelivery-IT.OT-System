4b936bebe7f99ef207822e63a682aa0cc65c10d7
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-09-13 11:00:46 +0200

    Added properties to OrderModel

--- FoodDelivery-IT.OT-System/Models/OrderModel.cs
+++ FoodDelivery-IT.OT-System/Models/OrderModel.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FoodDelivery_IT.OT_System.Models
 {
-    internal class OrderModel
+    public class OrderModel
     {
+        [Key]
+        public int OrderId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string CustomerName { get; set; } = "";  // who the order is for
+
+        [Required, MaxLength(100)]
+        public string DishName { get; set; } = "";      // what was ordered
+
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+
+        //public OrderStatus Status { get; set; } = OrderStatus.Placed;
+
+        public bool SentToOT { get; set; } = false; // has it been sent to OT system
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required, MaxLength(255)]
+        public string CreatedBy { get; set; } = ""; // staff who created order
     }
 }
